47458e309a49d9927c75f456210c88bb7fb1217f
ayushgangs <ayushaug2015@gmail.com>
2023-10-02 12:21:09 +0530

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let plus = document.querySelector('.plus')
 plus.addEventListener('click', popshow)
 
-let popup = document.querySelector('.popup')
+let popupWrap = document.querySelector('.popup-wrap')
 let inp1 = document.querySelector('.heading-inp1')
 let inp2 = document.querySelector('.heading-inp2')
 
 function popshow() {
-    popup.style.display = 'block'
+    popupWrap.style.display = 'flex'
+    popupWrap.style.alignItems = 'center'
+    popupWrap.style.justifyContent = 'center'
     inp1.value = ''
     inp2.value = ''
     let add = document.querySelector(".add")
@@ -19,19 +21,19 @@ let cross = document.querySelector('.cross')
 cross.addEventListener('click', pophide)
 
 function pophide() {
-    popup.style.display = 'none'
+    popupWrap.style.display = 'none'
 }
 
 
 function addbox() {
-    
+
     pophide()
-    
+
     let middle = document.querySelector(".middle")
     let oldhtml = middle.innerHTML
 
     middle.innerHTML = oldhtml +
-    ` 
+        ` 
         <div class="box">
             <div class="boxhead">
                 <div class="remaining-boxhead">
@@ -56,40 +58,34 @@ function addbox() {
 }
 
 
-function closeMinEditBox(){
+function closeMinEditBox() {
 
     let crossBtnArr = document.querySelectorAll('.box-cross')
     // console.log(crossBtnArr)
 
-    for(let i=0;i<crossBtnArr.length;i++)
-    {
-        crossBtnArr[i].addEventListener('click',hidebox)
+    for (let i = 0; i < crossBtnArr.length; i++) {
+        crossBtnArr[i].addEventListener('click', hidebox)
     }
 
-    function hidebox(e) {   
-        // 'e' is a pointer event object that keeps the complete info. of the event that is 
-        //  performed, in our case the 'click' event. It is passed as a parameter by JS to the function that is called
-        
-        // console.log(e.target)
-        // 'target' is a key inside 'e' object that contains the HTML of the element on which the event is performed
+    function hidebox(e) {
+        // 'e' is a pointer event object that keeps the complete info. of the event that is performed, in our case the 'click' event. It is passed as a parameter by JS to the function that is called
 
-        let box = e.target.closest(".box") // 'closest' is a predefined function that is used to select the HTML of the element whose class is provided
-                                           //  that is nearest to the target element. P.S. the target element should be the child of the element whose class
-                                           //  is provided to closest function
+        // console.log(e.target)   //'target' is a key inside 'e' object that contains the HTML of the element on which the event is performed
+
+        let box = e.target.closest(".box") // 'closest' is a predefined function that is used to select the HTML of the element whose class is provided that is nearest to the target element. P.S. the target element should be the child of the element whose class is provided to closest function
         box.style.display = "none"
     }
 
     let minBtnArr = document.querySelectorAll(".box-min")
     // console.log(minBtnArr)
 
-    for(let i=0; i<minBtnArr.length;i++)
-    {
-        minBtnArr[i].addEventListener('click',boxmin) 
+    for (let i = 0; i < minBtnArr.length; i++) {
+        minBtnArr[i].addEventListener('click', boxmin)
     }
 
     function boxmin(e) {
 
-        
+
         let box = e.target.closest(".box")
         // console.log(box)
         let boxbody = box.querySelector(".boxbody")
@@ -106,16 +102,16 @@ function closeMinEditBox(){
     let editIconArr = document.querySelectorAll(".boxhead-btns i")
     // console.log(editIconArr)
 
-    for(let i=0;i<editIconArr.length;i++)
-    {
-        editIconArr[i].addEventListener('click',boxedit)
+    for (let i = 0; i < editIconArr.length; i++) {
+        editIconArr[i].addEventListener('click', boxedit)
     }
 
-    function boxedit(e)
-    {
-        popup.style.display = 'block'
+    function boxedit(e) {
+        popupWrap.style.display = 'flex'
+        popupWrap.style.alignItems = 'center'
+        popupWrap.style.justifyContent = 'center'
         let add = document.querySelector(".add")
-        add.innerHTML= `<button class="editbtn">Edit</button>`
+        add.innerHTML = `<button class="editbtn">Save</button>`
 
 
         let box = e.target.closest(".box")
@@ -129,19 +125,19 @@ function closeMinEditBox(){
 
         let editbtn = add.querySelector(".editbtn")
         // console.log(editbtn)
-        editbtn.addEventListener('click',popedit)
+        editbtn.addEventListener('click', popedit)
 
 
-        function popedit(){
+        function popedit() {
             pophide()
-            
+
             remBoxhead.innerText = inp1.value
             boxbody.innerText = inp2.value
         }
 
     }
 
-    
+
 
 }
 
